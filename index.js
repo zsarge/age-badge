aa4367acd61cfc7724e8ebd2c1bf74aa16b3576e
zsarge <zackarywsargent@gmail.com>
2021-11-06 15:30:11 -0400

    add correct svg

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 "use strict";
 
 exports.handler = async (event) => {
-	const svg = `
-		<svg width="100" height="100">
-		  <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
-		</svg>
-	`;
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(svg),
-    };
-    return response;
+	// Just show a star as an example
+	const exampleSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">' + 
+		'<path d="M50,3l12,36h38l-30,22l11,36l-31-21l-31,21l11-36l-30-22h38z" ' + 
+		'fill="#FF0" stroke="#FC0" stroke-width="2"/>' + 
+		'</svg>';
+
+	const response = {
+		statusCode: 200,
+		body: exampleSvg,
+		headers: {
+			// Set the content type to the correct Mime type
+			'Content-Type': 'image/svg+xml',
+		},
+	};
+	return response;
 };
 
